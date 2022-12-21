f2c664559800eef68ae64276c0c51c96bf283730
sharukh <mohammedsharukh987@gmail.com>
2022-12-21 19:03:34 +0530

    first youtube-clone commit

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, ChannelCard } from "./";
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              " linear-gradient(90deg, rgba(102,128,25,1) 11%, rgba(9,121,36,1) 44%, rgba(23,169,23,1) 67%, rgba(49,255,0,1) 90%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
